6f737af63b60079e7c48bb8c8516d0c8a53eb9cd
Jack Halten Fahnestock <buttface@jackf.me>
2020-04-10 20:59:28 -0400

    update social media meta tags

--- src/head.js
+++ src/head.js
@@ -4,6 +4,7 @@ import {site, colors} from './components/config/vars'
 
 const public_url = 'https://post-contact.site/'
 const icon = 'assets/img/icon.png'
+const img = 'assets/img/meta_img.jpg'
 
 const Head = () => (
   <Helmet>
@@ -12,25 +13,21 @@ const Head = () => (
     <meta http-equiv='Accept-CH' content='DPR, Width, Viewport-Width'/>
     <meta name='viewport' content='width=device-width, initial-scale=1'/>
     <meta name='description' content={site.description}/>
-    <link rel='shortcut icon'
-      href={public_url + icon}/>
-    <link rel='apple-touch-icon'
-      href={public_url + icon}/>
-    <link rel='manifest' href={public_url + 'manifest.json'}/>
     <meta name='theme-color' content={colors.black}/>
+    <link rel='shortcut icon' href={public_url + icon}/>
+    <link rel='apple-touch-icon' href={public_url + icon}/>
+    <link rel='manifest' href={public_url + 'manifest.json'}/>
 
-    <meta property='og:title'
-      content={site.title}/>
-    <meta property='og:type'
-    content='website'/>
-    <meta property='og:url'
-      content={public_url}/>
-    <meta property='og:image'
-      content={public_url + icon}/>
-    <meta property='og:site_name'
-      content={site.title}/>
-    <meta property='og:description'
-      content={site.description}/>
+    <meta property='og:title' content={site.title}/>
+    <meta property='og:description' content={site.description}/>
+    <meta property='og:image' content={public_url + img}/>
+    <meta property='og:url' content={public_url}/>
+
+    {/* <meta name='twitter:title' content={site.title}/>
+      <meta name='twitter:description' content={site.description}/>
+    <meta name='twitter:image' content={public_url + img}/> */}
+    <meta name='twitter:card' content={public_url + img}/>
+
     <title>{site.title}</title>
   </Helmet>
 )
