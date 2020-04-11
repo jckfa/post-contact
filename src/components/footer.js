04097e236c167d35aff5f14d9e66b711053ac7bd
Jack Halten Fahnestock <buttface@jackf.me>
2020-04-11 06:05:57 -0400

    add urls for special thx ppl

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,6 +3,13 @@ import styled from 'styled-components'
 import {site} from './config/vars'
 import updated from './data/updated_time'
 
+const ramon_url = 'https://ramongd.com/',
+      paul_url  = 'https://soulellis.com/',
+      jac_url   = 'https://www.risd.edu/people/jacqueline-shaw/',
+      lucy_url  = 'https://lucindahitchcock.com/',
+      sue_url   = 'https://www.risd.edu/people/sue-mazzucco/'
+
+
 const Shell = styled.section`
   padding-top: 3em;
 `
@@ -16,15 +23,15 @@ const Visit = styled.p`
 const Footer = (props) => (
   <Shell>
     <Visit>
-      Visit the site in person at <a href={site.address_url} target="_blank" rel="noopener noreferrer">{site.address}</a> or on Instagram <a href={"https://instagram.com/" + site.instagram_handle} target="_blank" rel="noopener noreferrer">@{site.instagram_handle}</a>. And stay safe out there.
+      Visit the site in person at <a href={site.address_url} target='_blank' rel='noopener noreferrer'>{site.address}</a> or on Instagram <a href={'https://instagram.com/' + site.instagram_handle} target='_blank' rel='noopener noreferrer'>@{site.instagram_handle}</a>. And stay safe out there.
     </Visit>
 
     <p>
-      Special thanks to Stella Herzog for allowing the use of her building in this way; to Ramon Tejada, Paul Soulellis, and Jacqueline Shaw for their crucial feedback; and to Lucy Hitchcock and Sue Mazzucco for securing RISD GD funding for supplies necessary to make this project possible.
-    </p>
+      Special thanks to Stella Herzog for allowing the use of her building in this way; to <a href={ramon_url}  target='_blank' rel='noopener noreferrer'>Ramon Tejada</a>, <a href={paul_url}  target='_blank' rel='noopener noreferrer'>Paul Soulellis</a>, and <a href={jac_url} target='_blank' rel='noopener noreferrer'>Jacqueline Shaw</a> for their crucial feedback; and to <a href={lucy_url} target='_blank' rel='noopener noreferrer'>Lucy Hitchcock</a> and <a href={sue_url} target='_blank' rel='noopener noreferrer'>Sue Mazzucco</a> for securing RISD GD funding for supplies necessary to make this project possible.
+        </p>
 
-    <p>
-      Last updated: {updated} <a href="https://time.is/UTC" target="_blank" rel="noopener noreferrer">UTC</a> • <a href={site.source_code} target="_blank" rel="noopener noreferrer">Source Code</a>
+        <p>
+          Last updated: {updated} <a href='https://time.is/UTC' target='_blank' rel='noopener noreferrer'>UTC</a> • <a href={site.source_code} target='_blank' rel='noopener noreferrer'>Source Code</a>
     </p>
   </Shell>
 )
