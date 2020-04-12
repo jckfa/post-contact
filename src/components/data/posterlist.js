f1ba1524b8a83981ced98072203bcb9269cbb34a
Jack Halten Fahnestock <buttface@jackf.me>
2020-04-11 21:32:55 -0400

    add poster 001 images

--- src/components/data/posterlist.js
+++ src/components/data/posterlist.js
@@ -1,5 +1,32 @@
 const posterList = [
   {
+    image: '001_01.jpg',
+    id: 1,
+    author: 'Jack Halten Fahnestock',
+    author_url: 'https://jackf.me',
+    size: '30"W×40"H',
+    install_date: '8 April 2020'
+  },
+
+  {
+    image: '001_02.jpg',
+    id: 1,
+    author: 'Jack Halten Fahnestock',
+    author_url: 'https://jackf.me',
+    size: '30"W×40"H',
+    install_date: '8 April 2020'
+  },
+
+  {
+    image: '001_03.jpg',
+    id: 1,
+    author: 'Jack Halten Fahnestock',
+    author_url: 'https://jackf.me',
+    size: '30"W×40"H',
+    install_date: '8 April 2020'
+  },
+  
+  {
     image: '000_01.jpg',
     id: 0,
     author: 'Contributor Name',
@@ -25,33 +52,6 @@ const posterList = [
     size: '30"W×40"H',
     install_date: '2 April 2020'
   },
-
-  // {
-  //   image: '001_01.jpg',
-  //   id: 1,
-  //   author: 'Jack Halten Fahnestock',
-  //   author_url: 'https://jackf.me',
-  //   size: '30"W×40"H',
-  //   install_date: '8 April 2020'
-  // },
-  //
-  // {
-  //   image: '001_02.jpg',
-  //   id: 1,
-  //   author: 'Jack Halten Fahnestock',
-  //   author_url: 'https://jackf.me',
-  //   size: '30"W×40"H',
-  //   install_date: '8 April 2020'
-  // },
-  //
-  // {
-  //   image: '001_03.jpg',
-  //   id: 1,
-  //   author: 'Jack Halten Fahnestock',
-  //   author_url: 'https://jackf.me',
-  //   size: '30"W×40"H',
-  //   install_date: '8 April 2020'
-  // },
 ]
 
 export default posterList
