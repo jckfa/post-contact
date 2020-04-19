100cc2e92d7a7bb294675222502a4c8fcf110a52
Jack Halten Fahnestock <buttface@jackf.me>
2020-04-19 15:12:39 -0400

    add new poster

--- src/components/data/posterlist.js
+++ src/components/data/posterlist.js
@@ -1,5 +1,32 @@
 const posterList = [
   {
+    image: '004_01.jpg',
+    id: 4,
+    author: 'Jack Halten Fahnestock',
+    author_url: 'https://jackf.me/',
+    size: '30"W×40"H',
+    install_date: '19 April 2020'
+  },
+
+  {
+    image: '004_02.jpg',
+    id: 4,
+    author: 'Jack Halten Fahnestock',
+    author_url: 'https://jackf.me/',
+    size: '30"W×40"H',
+    install_date: '19 April 2020'
+  },
+
+  {
+    image: '004_03.jpg',
+    id: 4,
+    author: 'Jack Halten Fahnestock',
+    author_url: 'https://jackf.me/',
+    size: '30"W×40"H',
+    install_date: '19 April 2020'
+  },
+
+  {
     image: '003_01.jpg',
     id: 3,
     author: 'Fabian Fohrer',
