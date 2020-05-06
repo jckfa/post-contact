633be5a8dbe34d7635dfb3ac2ad480b35869ba98
Jack Halten Fahnestock <buttface@jackf.me>
2020-05-05 20:13:58 -0400

    update google maps url to proper Post-Contact "business" profile

--- src/components/config/vars.js
+++ src/components/config/vars.js
@@ -5,7 +5,7 @@ export const site = {
   email_subject: 'Post-Contact Submission',
   email_body: `Attach your B&W PDF/PNG/JPG. Please include 1) first & last name, 2) website URL and/or Instagram handle, 3) title [optional; defaults to Post-Contact ###], 4) description/statement [optional but highly encouraged], and 5) any additional comments if necessary. All attempts will be made to respect your material as is, however minor modifications may be necessary, including halftoning, rescaling, and repeating. Hopefully this adds a favorable element of uncertainty to the contributing process. Thank you for participating! Contributions are essential to the life of this project.`,
   address: '104 Sheldon St',
-  address_url: 'https://goo.gl/maps/tHuP9scahLKj8Cdw5',
+  address_url: 'https://g.page/postcontact',
   instagram_handle: 'post_contact',
   source_code: 'https://github.com/jckfa/post-contact',
 }
