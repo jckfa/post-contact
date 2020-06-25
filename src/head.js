2d4fb43cfda49b95dedd289d4c3dcca2ed9c98ad
Jack Halten Fahnestock <buttface@jackf.me>
2020-06-24 22:55:50 -0400

    fix caching issue

--- src/head.js
+++ src/head.js
@@ -15,6 +15,8 @@ const Head = () => (
     <meta charset='utf-8'/>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'/>
     <meta http-equiv='Accept-CH' content='DPR, Width, Viewport-Width'/>
+    <meta http-equiv='Pragma' content='no-cache'/>
+    <meta http-equiv='Expires' content='0'/>
     <meta name='viewport' content='width=device-width, initial-scale=1'/>
     <meta name='description' content={description}/>
     <meta name='theme-color' content={colors.black}/>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { render } from 'react-snapshot'
 import App from './App'
 import './components/config/global'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
+import { unregister as unregisterServiceWorker } from './registerServiceWorker'
 
 render(
   <App />,
   document.querySelector('#root')
 )
 
-registerServiceWorker()
+// registerServiceWorker()
+unregisterServiceWorker()
