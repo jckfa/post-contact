1a76f5df3229875970a8bd9bcbf6e10333016f61
Jack Halten Fahnestock <buttface@jackf.me>
2020-06-25 23:51:49 -0400

    register service worker

--- src/index.js
+++ src/index.js
@@ -11,9 +11,7 @@ render(
   document.querySelector('#root')
 )
 
-// registerServiceWorker()
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
